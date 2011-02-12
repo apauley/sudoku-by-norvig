96c3fc6707925dd9f157ba5b4c8b787a62ae598c
Andreas Pauley <apauley+github@gmail.com>
2011-02-12 17:49:10 +0200

    Initial commit

---
+++ sudoko.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+def cross(A, B):
+    return [a+b for a in A for b in B]
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+digits   = '123456789'
+squares  = cross(rows, cols)
+
+column_squares = [cross(rows, c) for c in cols]
+row_squares    = [cross(r, cols) for r in rows]
+box_squares    = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+unitlist = (column_squares +
+            row_squares +
+            box_squares)
+
+units = dict((s, [u for u in unitlist if s in u]) 
+             for s in squares)
+peers = dict((s, set(s2 for u in units[s] for s2 in u if s2 != s))
+             for s in squares)
