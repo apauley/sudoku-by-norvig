8e5d5111cefd13c7f90c0459e9d26e5dbb76d2fa
Andreas Pauley <apauley+github@gmail.com>
2011-02-12 18:01:16 +0200

    Some reshuffelings

--- sudoko.py
+++ sudoko.py
@@ -3,20 +3,22 @@
 def cross(A, B):
     return [a+b for a in A for b in B]
 
+digits = '123456789'
 rows = 'ABCDEFGHI'
-cols = '123456789'
-digits   = '123456789'
-squares  = cross(rows, cols)
+cols = digits
+squares = cross(rows, cols)
 
 column_squares = [cross(rows, c) for c in cols]
 row_squares    = [cross(r, cols) for r in rows]
-box_squares    = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+box_squares    = [cross(rs, cs)
+                  for rs in ('ABC','DEF','GHI')
+                  for cs in ('123','456','789')]
 
 unitlist = (column_squares +
             row_squares +
             box_squares)
 
-units = dict((s, [u for u in unitlist if s in u]) 
+units = dict((s, [u for u in unitlist if s in u])
              for s in squares)
 peers = dict((s, set(s2 for u in units[s] for s2 in u if s2 != s))
              for s in squares)
